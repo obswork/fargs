e3ee539578af6a4250f2e25228828d0aebb62850
obswork <obssabizuwork@gmail.com>
2016-05-14 23:43:55 -0400

    changing timezone

--- fargs/settings/base.py
+++ fargs/settings/base.py
@@ -149,7 +149,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
