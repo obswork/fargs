65bf64e000f675c896798758a76e23eb6677a1a0
obswork <obssabizuwork@gmail.com>
2016-05-16 17:22:25 -0400

    refactoring js

---
+++ home/static/home/js/form.js
@@ -0,0 +1,31 @@
+$("#sendMessage").click(function(e){
+    e.preventDefault();
+    var contactMessage = $("#contactForm").serialize();
+    $.ajax({
+        type: "POST",
+        url: "/",
+        data: contactMessage,
+        success: function(data){
+            if(data.success){
+                $('#contactForm')[0].reset();
+                $("#contactForm").find(".error").remove();
+                $("#contactForm").before('<div class="alert success">' + data.success + '</div>');
+                setTimeout(function(){
+                    $(".success").fadeOut("slow");
+                }, 6000);
+            }else{
+                $("#contactForm").find(".error").remove();
+                for (var key in data) {
+                    if (key in {email: 1, message: 1, number: 1, sender: 1, }) {
+                        error = data[key][0];
+                        field = $("#contactForm").find("#id_" + key);
+                        field.after('<div class="alert error">' + error + '</div>');
+                    }
+                }
+            }
+        }
+    });
+});
+$("#clear").click(function(e){
+    $("#contactForm").find(".error").remove();
+});
